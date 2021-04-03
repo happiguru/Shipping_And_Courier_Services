f4ff6a2b0143606dc4e8afcac5fe19bf0507539e
happiguru <hguruman@gmail.com>
2021-04-03 12:26:53 +0400

    Update contact page

--- views/contact.php
+++ views/contact.php
@@ -1,3 +1,35 @@
+<?php
+
+if(isset($_POST['submit'])){
+   
+  $name=$_REQUEST['name'];
+  $email=$_REQUEST['email'];
+  $url=$_REQUEST['url'];
+  $company=$_REQUEST['organization'];
+  $position=$_REQUEST['position'];
+  $country=$_REQUEST['country'];
+  $messages=$_REQUEST['message'];
+
+  $message=("
+  Name: $name,
+  Email: $email,
+  Website: $url,
+  Company: $company,
+  Position: $position,
+  Country: $country,
+  Message: $messages.");
+
+  $to="hguruman@gmail.com";
+  $subject="FeedBack Form";
+  $headers = "From: $email"; 
+  $sent = mail($to, $subject, $message, $headers) ; 
+  if($sent){
+  echo("<center>Your Feedback was sent successfully</center>");
+  }else{
+  echo("<center>We encountered an error sending your mail</center>");
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -29,205 +61,199 @@
       </ol>
     </div>
 </section>
+<section class="contact-wrap section-padding">
+  <div class="container">
+    <div class="send-feedback">
+      <h2>Send us your feedback</h2>
+      <form id="contactForm" method="POST">
+        <div class="row">
+        <div class="col-12 col-md-4">
+            <div class="form-group">
+              <label for="name">Name*</label>
+              <input id="name" name="name" type="text" class="form-control" required="" placeholder="">
+            </div>
+          </div>
+          <div class="col-12 col-md-4">
+            <div class="form-group">
+              <label for="email">E-mail*</label>
+              <input id="email" name="email" type="email" class="form-control" required="" placeholder="">
+            </div>
+          </div><!-- /.col -->
+          <div class="col-12 col-md-4">
+            <div class="form-group">
+              <label for="url">Website</label>
+              <input id="url" name="url" type="text" class="form-control" placeholder="">
+            </div>
+          </div><!-- /.col -->
+          <div class="col-12 col-md-4">
+            <div class="form-group">
+              <label for="organization">Organization</label>
+              <input id="organization" name="organization" type="text" class="form-control" placeholder="">
+            </div>
+          </div><!-- /.col -->
+          <div class="col-12 col-md-4">
+            <div class="form-group">
+              <label for="position">Position</label>
+              <input id="position" name="position" type="text" class="form-control" placeholder="">
+            </div>
+          </div><!-- /.col -->
+          <div class="col-12 col-md-4">
+            <div class="form-group">
+              <label for="country">Country</label>
+              <input id="country" name="country" type="text" class="form-control" placeholder="">
+            </div>
+          </div><!-- /.col -->
+            <div class="col-12">
+              <div class="form-group">
+                  <label>Message</label>
+                  <textarea id="message" name="message" class="form-control" rows="2" required="" placeholder=""></textarea>
+              </div>
+          </div><!-- /.col -->
+          <div class="col-12">
+          <div class="form-group submit-btn">
+            <input type="submit" name="submit" class="btn btn-primary" value="Send Feedback">
+          </div>
+          </div>
+        </div>
+      </form>
+    </div><!-- /.send-feedback -->
+    <div class="office-locations">
+      <h2>Worldwide Location</h2>
+      <div class="row">
+        <div class="col-sm-3 col-xs-6">
+          <div class="office-address">
+            <h3>Head Office</h3>
+            <address>
+              <span>384 Maple Circle</span>
+              <span>Simi Valley Navada 47489</span>
+              <br>
+              <span>T.(554) 616 9926</span>
+              <span>M.(554) 616 9926</span>
+              <span>F.(554) 616 9926</span>
+              <a href="#">example@yahoo.com</a>
+
+            </address>
+          </div>
+        </div><!-- /.col -->
+
+        <div class="col-sm-3 col-xs-6">
+          <div class="office-address">
+            <h3>State Office</h3>
+            <address>
+              <span>384 Maple Circle</span>
+              <span>Simi Valley Navada 47489</span>
+              <br>
+              <span>T.(554) 616 9926</span>
+              <span>M.(554) 616 9926</span>
+              <span>F.(554) 616 9926</span>
+              <a href="#">example@yahoo.com</a>
+
+            </address>
+          </div>
+        </div><!-- /.col -->
+
+        <div class="col-sm-3 col-xs-6">
+          <div class="office-address">
+            <h3>Country Office</h3>
+            <address>
+              <span>384 Maple Circle</span>
+              <span>Simi Valley Navada 47489</span>
+              <br>
+              <span>T.(554) 616 9926</span>
+              <span>M.(554) 616 9926</span>
+              <span>F.(554) 616 9926</span>
+              <a href="#">example@yahoo.com</a>
+
+            </address>
+          </div>
+        </div><!-- /.col -->
+
+        <div class="col-sm-3 col-xs-6">
+          <div class="office-address">
+            <h3>Zonal Office</h3>
+            <address>
+              <span>384 Maple Circle</span>
+              <span>Simi Valley Navada 47489</span>
+              <br>
+              <span>T.(554) 616 9926</span>
+              <span>M.(554) 616 9926</span>
+              <span>F.(554) 616 9926</span>
+              <a href="#">example@yahoo.com</a>
+
+            </address>
+          </div>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+
+      <div class="row">
+        <div class="col-sm-3 col-xs-6">
+          <div class="office-address">
+            <h3>USA Office</h3>
+            <address>
+              <span>384 Maple Circle</span>
+              <span>Simi Valley Navada 47489</span>
+              <br>
+              <span>T.(554) 616 9926</span>
+              <span>M.(554) 616 9926</span>
+              <span>F.(554) 616 9926</span>
+              <a href="#">example@yahoo.com</a>
+
+            </address>
+          </div>
+        </div><!-- /.col -->
+
+        <div class="col-sm-3 col-xs-6">
+          <div class="office-address">
+            <h3>Atlanta Office</h3>
+            <address>
+              <span>384 Maple Circle</span>
+              <span>Simi Valley Navada 47489</span>
+              <br>
+              <span>T.(554) 616 9926</span>
+              <span>M.(554) 616 9926</span>
+              <span>F.(554) 616 9926</span>
+              <a href="#">example@yahoo.com</a>
+
+            </address>
+          </div>
+        </div><!-- /.col -->
+
+        <div class="col-sm-3 col-xs-6">
+          <div class="office-address">
+            <h3>Singapore Office</h3>
+            <address>
+              <span>384 Maple Circle</span>
+              <span>Simi Valley Navada 47489</span>
+              <br>
+              <span>T.(554) 616 9926</span>
+              <span>M.(554) 616 9926</span>
+              <span>F.(554) 616 9926</span>
+              <a href="#">example@yahoo.com</a>
+
+            </address>
+          </div>
+        </div><!-- /.col -->
+
+        <div class="col-sm-3 col-xs-6">
+          <div class="office-address">
+            <h3>Canada Office</h3>
+            <address>
+              <span>384 Maple Circle</span>
+              <span>Simi Valley Navada 47489</span>
+              <br>
+              <span>T.(554) 616 9926</span>
+              <span>M.(554) 616 9926</span>
+              <span>F.(554) 616 9926</span>
+              <a href="#">example@yahoo.com</a>
+
+            </address>
+          </div>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.office-locations -->
+  </div><!-- /.container -->
+</section>
 <?php
     include_once("map.php");
-?>
-<section class="contact-wrap section-padding">
-						<div class="container">
-							<div class="office-locations">
-								<h2>Worldwide Location</h2>
-								<div class="row">
-									<div class="col-sm-3 col-xs-6">
-										<div class="office-address">
-											<h3>Head Office</h3>
-											<address>
-												<span>384 Maple Circle</span>
-												<span>Simi Valley Navada 47489</span>
-												<br>
-												<span>T.(554) 616 9926</span>
-												<span>M.(554) 616 9926</span>
-												<span>F.(554) 616 9926</span>
-												<a href="#">example@yahoo.com</a>
-
-											</address>
-										</div>
-									</div><!-- /.col -->
-
-									<div class="col-sm-3 col-xs-6">
-										<div class="office-address">
-											<h3>State Office</h3>
-											<address>
-												<span>384 Maple Circle</span>
-												<span>Simi Valley Navada 47489</span>
-												<br>
-												<span>T.(554) 616 9926</span>
-												<span>M.(554) 616 9926</span>
-												<span>F.(554) 616 9926</span>
-												<a href="#">example@yahoo.com</a>
-
-											</address>
-										</div>
-									</div><!-- /.col -->
-
-									<div class="col-sm-3 col-xs-6">
-										<div class="office-address">
-											<h3>Country Office</h3>
-											<address>
-												<span>384 Maple Circle</span>
-												<span>Simi Valley Navada 47489</span>
-												<br>
-												<span>T.(554) 616 9926</span>
-												<span>M.(554) 616 9926</span>
-												<span>F.(554) 616 9926</span>
-												<a href="#">example@yahoo.com</a>
-
-											</address>
-										</div>
-									</div><!-- /.col -->
-
-									<div class="col-sm-3 col-xs-6">
-										<div class="office-address">
-											<h3>Zonal Office</h3>
-											<address>
-												<span>384 Maple Circle</span>
-												<span>Simi Valley Navada 47489</span>
-												<br>
-												<span>T.(554) 616 9926</span>
-												<span>M.(554) 616 9926</span>
-												<span>F.(554) 616 9926</span>
-												<a href="#">example@yahoo.com</a>
-
-											</address>
-										</div>
-									</div><!-- /.col -->
-								</div><!-- /.row -->
-
-								<div class="row">
-									<div class="col-sm-3 col-xs-6">
-										<div class="office-address">
-											<h3>USA Office</h3>
-											<address>
-												<span>384 Maple Circle</span>
-												<span>Simi Valley Navada 47489</span>
-												<br>
-												<span>T.(554) 616 9926</span>
-												<span>M.(554) 616 9926</span>
-												<span>F.(554) 616 9926</span>
-												<a href="#">example@yahoo.com</a>
-
-											</address>
-										</div>
-									</div><!-- /.col -->
-
-									<div class="col-sm-3 col-xs-6">
-										<div class="office-address">
-											<h3>Atlanta Office</h3>
-											<address>
-												<span>384 Maple Circle</span>
-												<span>Simi Valley Navada 47489</span>
-												<br>
-												<span>T.(554) 616 9926</span>
-												<span>M.(554) 616 9926</span>
-												<span>F.(554) 616 9926</span>
-												<a href="#">example@yahoo.com</a>
-
-											</address>
-										</div>
-									</div><!-- /.col -->
-
-									<div class="col-sm-3 col-xs-6">
-										<div class="office-address">
-											<h3>Singapore Office</h3>
-											<address>
-												<span>384 Maple Circle</span>
-												<span>Simi Valley Navada 47489</span>
-												<br>
-												<span>T.(554) 616 9926</span>
-												<span>M.(554) 616 9926</span>
-												<span>F.(554) 616 9926</span>
-												<a href="#">example@yahoo.com</a>
-
-											</address>
-										</div>
-									</div><!-- /.col -->
-
-									<div class="col-sm-3 col-xs-6">
-										<div class="office-address">
-											<h3>Canada Office</h3>
-											<address>
-												<span>384 Maple Circle</span>
-												<span>Simi Valley Navada 47489</span>
-												<br>
-												<span>T.(554) 616 9926</span>
-												<span>M.(554) 616 9926</span>
-												<span>F.(554) 616 9926</span>
-												<a href="#">example@yahoo.com</a>
-
-											</address>
-										</div>
-									</div><!-- /.col -->
-								</div><!-- /.row -->
-							</div><!-- /.office-locations -->
-
-							<div class="send-feedback">
-								<h2>Send us your feedback</h2>
-								<form id="contactForm" action="sendemail.php" method="POST">
-								  	<div class="row">
-								  	  	<div class="col-sm-4 col-12">
-										  <div class="form-group">
-										    <label for="name">Name*</label>
-										    <input id="name" name="name" type="text" class="form-control" required="" placeholder="">
-										  </div>
-									 	</div><!-- /.col -->
-									 	<div class="col-sm-4 col-12">
-										  <div class="form-group">
-										    <label for="email">E-mail*</label>
-										    <input id="email" name="email" type="email" class="form-control" required="" placeholder="">
-										  </div>
-										</div><!-- /.col -->
-										<div class="col-sm-4 col-12">
-										  <div class="form-group">
-										    <label for="url">Website</label>
-										    <input id="url" name="url" type="text" class="form-control" placeholder="">
-										  </div>
-										</div><!-- /.col -->
-								  	</div><!-- /.row -->
-								  	<div class="row">
-								  	  	<div class="col-sm-4 col-12">
-										  <div class="form-group">
-										    <label for="organization">Organization</label>
-										    <input id="organization" name="organization" type="text" class="form-control" placeholder="">
-										  </div>
-									 	</div><!-- /.col -->
-									 	<div class="col-sm-4 col-12">
-										  <div class="form-group">
-										    <label for="position">Position</label>
-										    <input id="position" name="position" type="text" class="form-control" placeholder="">
-										  </div>
-										</div><!-- /.col -->
-										<div class="col-sm-4 col-12">
-										  <div class="form-group">
-										    <label for="country">Country</label>
-										    <input id="country" name="country" type="text" class="form-control" placeholder="">
-										  </div>
-										</div><!-- /.col -->
-								  	</div><!-- /.row -->
-								  	<div class="row">
-                      <div class="col-12">
-                        <div class="form-group">
-                            <label>Message</label>
-                            <textarea id="message" name="message" class="form-control" rows="6" required="" placeholder=""></textarea>
-                        </div>
-										</div><!-- /.col -->
-								  	</div><!-- /.row -->
-
-								  	<div class="form-group submit-btn">
-								 	 	<button type="submit" class="btn btn-primary">Send feedback</button>
-								  	</div>
-								</form>
-							</div><!-- /.send-feedback -->
-						</div><!-- /.container -->
-					</section>
-<?php
     require_once('footer.php')
 ?>
